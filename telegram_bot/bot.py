d16bd486e26b79e48db42076a82c4d6ded993e69
Farhad-IT <yskakfarhad2010@gmail.com>
2026-06-09 00:37:24 +0500

    Clear

--- telegram_bot/bot.py
+++ telegram_bot/bot.py
@@ -124,11 +124,3 @@ async def show_subscriptions(message: Message):
             )
 
         await message.answer(text)
-
-
-#async def main():
-#    await dp.start_polling(bot)
-
-
-#if __name__ == '__main__':
-#    asyncio.run(main())
